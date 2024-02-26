289f298e6f28846b4be0a526efa3cac02b29b849
Ubaldo <150012434+UbaldoCap@users.noreply.github.com>
2024-02-26 16:40:33 +0100

    Add files via upload

---
+++ demoCors/src/test/java/com/example/demoCors/DemoCorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoCors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
